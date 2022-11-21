60c0c2bb7e0cd1eea6c39b7e94d7605c4840c25c
张露 <62973206+zl-go@users.noreply.github.com>
2022-11-21 10:16:44 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@
 files=$(ls apps/)
 for file in $files
 do
-  sed -i 's/restart: always/restart: unless-stopped/g' apps/$file/docker-compose.yml
+  if [-e apps/$file/docker-compose.yml];then
+    sed -i 's/restart: always/restart: unless-stopped/g' apps/$file/docker-compose.yml
+  fi
 done
