4fcaf1e4b1e0498b465e4c11e1f1153ad2cce9cb
张露 <62973206+zl-go@users.noreply.github.com>
2022-11-21 10:24:27 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 files=$(ls apps/)
 for file in $files
 do
-  if [-e apps/$file/docker-compose.yml];then
+  if [-f apps/$file/docker-compose.yml];then
     sed -i 's/restart: always/restart: unless-stopped/g' apps/$file/docker-compose.yml
   else 
     echo "apps/$file/docker-compose.yml dosen't exist"
